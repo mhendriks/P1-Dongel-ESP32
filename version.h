1ef66f8b58984c2fa1fa49e2db4c2926e3ae204d
Martijn Hendriks <m.a.hendriks@gmail.com>
2022-05-17 22:55:21 +0200

    bump

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 4
-#define _VERSION_MINOR 1
-#define _VERSION_PATCH 1
-#define _VERSION_BUILD 498
-#define _VERSION_DATE "24/04/2022"
-#define _VERSION_TIME "15:08:28"
-#define _VERSION_ONLY "4.1.1"
-#define _VERSION_NOBUILD "4.1.1 (24/04/2022)"
-#define _VERSION "4.1.1+498 (24/04/2022)"
+#define _VERSION_MINOR 2
+#define _VERSION_PATCH 0
+#define _VERSION_BUILD 577
+#define _VERSION_DATE "17/05/2022"
+#define _VERSION_TIME "21:59:42"
+#define _VERSION_ONLY "4.2.0"
+#define _VERSION_NOBUILD "4.2.0 (17/05/2022)"
+#define _VERSION "4.2.0+577 (17/05/2022)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
