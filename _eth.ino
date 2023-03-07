fe88dbb18ed3da5c536bd5d0ba0972ebc3f53d59
mhendriks <m.a.hendriks@gmail.com>
2023-03-07 23:16:05 +0100

    add ethernet version

---
+++ _eth.ino
@@ -0,0 +1,90 @@
+#ifdef ETHERNET
+
+#define INT_GPIO            1
+#define MISO_GPIO           5
+#define MOSI_GPIO           6
+#define SCK_GPIO            4
+#define CS_GPIO             10
+
+#include <WebServer_ESP32_SC_W5500.h>
+
+#define NUMBER_OF_MAC      20
+
+byte mac[][NUMBER_OF_MAC] =
+{
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x02 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x03 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x04 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x05 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x06 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x07 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x08 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x09 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0A },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0B },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0C },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0D },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x0E },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x0F },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x10 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x11 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x12 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x13 },
+  { 0xDE, 0xAD, 0xBE, 0xEF, 0xBE, 0x14 },
+};
+
+// Select the IP address according to your local network
+IPAddress myIP(192, 168, 2, 232);
+IPAddress myGW(192, 168, 2, 1);
+IPAddress mySN(255, 255, 255, 0);
+
+// Google DNS Server IP
+IPAddress myDNS(8, 8, 8, 8);
+
+void startETH(){
+// To be called before ETH.begin()
+  ESP32_W5500_onEvent();
+
+  // start the ethernet connection and the server:
+  // Use DHCP dynamic IP and random mac
+  uint16_t index = millis() % NUMBER_OF_MAC;
+
+  //bool begin(int MISO_GPIO, int MOSI_GPIO, int SCLK_GPIO, int CS_GPIO, int INT_GPIO, int SPI_CLOCK_MHZ,
+  //           int SPI_HOST, uint8_t *W5500_Mac = W5500_Default_Mac);
+  //ETH.begin( MISO_GPIO, MOSI_GPIO, SCK_GPIO, CS_GPIO, INT_GPIO, SPI_CLOCK_MHZ, ETH_SPI_HOST );
+  ETH.begin( MISO_GPIO, MOSI_GPIO, SCK_GPIO, CS_GPIO, INT_GPIO, SPI_CLOCK_MHZ, ETH_SPI_HOST, mac[index] );
+
+  // Static IP, leave without this line to get IP via DHCP
+  //bool config(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dns1 = 0, IPAddress dns2 = 0);
+  //ETH.config(myIP, myGW, mySN, myDNS);
+
+  ESP32_W5500_waitForConnect();
+
+  PostMacIP(); //post mac en ip 
+
+  USBSerial.print(F("ETH IP : ")); //CDC output
+  USBSerial.println(IP_Address());
+  USBSerial.print(F("ETH MAC: ")); //CDC output
+  USBSerial.println(MAC_Address());
+
+  SwitchLED( LED_ON, BLUE ); //Ethernet available = RGB LED Blue
+}
+
+#endif
+
+String IP_Address(){
+#ifdef ETHERNET
+  return ETH.localIP().toString();
+#else
+  return WiFi.localIP().toString();
+#endif
+}
+
+String MAC_Address(){
+#ifdef ETHERNET
+  return ETH.macAddress();
+#else
+  return WiFi.macAddress();
+#endif
+}
