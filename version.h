be980f67190d3bd369f8d58e2f095a4dcd0c5266
mhendriks <m.a.hendriks@gmail.com>
2023-06-16 13:14:42 +0200

    version bump

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define _VERSION_MAJOR 4
 #define _VERSION_MINOR 8
 #define _VERSION_PATCH 4
-#define _VERSION_BUILD 1822
-#define _VERSION_DATE "26/05/2023"
-#define _VERSION_TIME "08:12:29"
+#define _VERSION_BUILD 1869
+#define _VERSION_DATE "15/06/2023"
+#define _VERSION_TIME "22:47:55"
 #define _VERSION_ONLY "4.8.4"
-#define _VERSION_NOBUILD "4.8.4 (26/05/2023)"
-#define _VERSION "4.8.4+1822 (26/05/2023)"
+#define _VERSION_NOBUILD "4.8.4 (15/06/2023)"
+#define _VERSION "4.8.4+1869 (15/06/2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
