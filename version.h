37dc71af35e3345cefe3b943bbc092af907dc4e8
mhendriks <m.a.hendriks@gmail.com>
2023-09-14 10:22:14 +0200

    fix water mbus store

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 4
 #define _VERSION_MINOR 8
-#define _VERSION_PATCH 4
-#define _VERSION_BUILD 1817
-#define _VERSION_DATE "28/08/2023"
-#define _VERSION_TIME "23:37:15"
-#define _VERSION_ONLY "4.8.4"
-#define _VERSION_NOBUILD "4.8.4 (28/08/2023)"
-#define _VERSION "4.8.4+1817 (28/08/2023)"
+#define _VERSION_PATCH 5
+#define _VERSION_BUILD 1818
+#define _VERSION_DATE "13/09/2023"
+#define _VERSION_TIME "12:13:24"
+#define _VERSION_ONLY "4.8.5"
+#define _VERSION_NOBUILD "4.8.5 (13/09/2023)"
+#define _VERSION "4.8.5+1818 (13/09/2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
