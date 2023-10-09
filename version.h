691f277c7a9fc26e49496672a1b66309adab743d
mhendriks <m.a.hendriks@gmail.com>
2023-10-09 13:22:41 +0200

    version bump (new build with new jwt)

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define _VERSION_MAJOR 4
 #define _VERSION_MINOR 8
 #define _VERSION_PATCH 6
-#define _VERSION_BUILD 1819
-#define _VERSION_DATE "08/10/2023"
-#define _VERSION_TIME "08:04:05"
+#define _VERSION_BUILD 1820
+#define _VERSION_DATE "09/10/2023"
+#define _VERSION_TIME "13:11:19"
 #define _VERSION_ONLY "4.8.6"
-#define _VERSION_NOBUILD "4.8.6 (08/10/2023)"
-#define _VERSION "4.8.6+1819 (08/10/2023)"
+#define _VERSION_NOBUILD "4.8.6 (09/10/2023)"
+#define _VERSION "4.8.6+1820 (09/10/2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
