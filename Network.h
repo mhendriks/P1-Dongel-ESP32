0729b0c87c48abeb5e58efe381813c5384d26d0b
mhendriks <m.a.hendriks@gmail.com>
2023-10-09 13:07:48 +0200

    auto wifi reconnect

--- Network.h
+++ Network.h
@@ -78,7 +78,7 @@ static void onWifiEvent (WiFiEvent_t event) {
         if (DUE(WifiReconnect)) {
           if ( WifiConnected ) LogFile("Wifi connection lost",true); //log only once 
           WifiConnected = false;                 
-          WiFi.reconnect();
+//          WiFi.reconnect();
         }
         break;
     default:
@@ -116,6 +116,7 @@ void startWiFi(const char* hostname, int timeOut)
   manageWiFi.setShowDnsFields(true);    // force show dns field always  
   manageWiFi.setRemoveDuplicateAPs(false);
   manageWiFi.setScanDispPerc(true); // display percentages instead of graphs for RSSI
+  manageWiFi.setWiFiAutoReconnect(true);
 
   //add custom html at inside <head> for all pages -> show pasessword function
 //  manageWiFi.setCustomHeadElement("<script>function f() {var x = document.getElementById('p');x.type==='password'?x.type='text':x.type='password';}</script>");
