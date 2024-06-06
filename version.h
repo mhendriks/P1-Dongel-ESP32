08694f49ee20349fb5757179e506e949692e9016
mhendriks <m.a.hendriks@gmail.com>
2024-06-06 10:08:44 +0200

    version bump

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 4
-#define _VERSION_MINOR 8
-#define _VERSION_PATCH 14
+#define _VERSION_MINOR 9
+#define _VERSION_PATCH 0
 #define _VERSION_BUILD 2808
 #define _VERSION_DATE "15/01/2024"
 #define _VERSION_TIME "12:53:21"
