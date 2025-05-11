50aa917523e4f042c58d155da121fd6e833523ad
mhendriks <m.a.hendriks@gmail.com>
2025-05-11 22:20:46 +0200

    fix: burnup chart

--- cdn/burnup.js
+++ cdn/burnup.js
@@ -2,7 +2,7 @@
 		plugin by Mar10us
 */
 
-const APIGW = window.location.protocol+'//'+window.location.host+'/api/';
+const // APIGW = window.location.protocol+'//'+window.location.host+'/api/';
 const URL_HISTORY_HOURS = APIGW + "../RNGhours.json";
 const URL_HISTORY_DAYS = APIGW + "../RNGdays.json";
 const URL_HISTORY_MONTHS = APIGW + "../RNGmonths.json";
